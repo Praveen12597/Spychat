98d2de782134f71899daec513cc4ab0a4f1bc2ca
praveen verma <praveen.kumar9198082884@gmail.com>
2018-03-25 00:48:42 +0530

    Started the project spychat and take the input from The Spy

---
+++ main.py
@@ -0,0 +1,7 @@
+print "Welcome to SpyChat"
+# Asking for name of Spy
+spy_name = raw_input("Enter your Spy Name : ")
+print "Welcome " + spy_name + " glad to have you back with us."
+# Asking for the SALUTATION from the spy
+salutation = raw_input("What should we call you (Mr. or Ms.) : ")
+print "Alright " + salutation + " " + spy_name + " I'd like to know a little bit more about you..."
